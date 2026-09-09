3f5ff9e0b0ecb84449eee139f3b5c1ebaa558bbb
jeremstyke <juryjeremy@gmail.com>
2026-09-09 13:25:44 +0000

    Fix build: missing using System.IO in AnalyzeViewModel and LargeFilesViewModel

--- src/FreeDiskAnalyzer/ViewModels/LargeFilesViewModel.cs
+++ src/FreeDiskAnalyzer/ViewModels/LargeFilesViewModel.cs
@@ -1,6 +1,7 @@
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.IO;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using FreeDiskAnalyzer.Core.Models;
