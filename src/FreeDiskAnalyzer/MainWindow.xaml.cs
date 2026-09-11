2d273ae60bcccaf9ca921a70ce646e1db7f8db31
jeremstyke <juryjeremy@gmail.com>
2026-09-11 10:05:34 +0000

    Add polish animations: smooth button hover/press transitions, animated sidebar nav highlight, fade-in transition when switching tabs

--- src/FreeDiskAnalyzer/MainWindow.xaml.cs
+++ src/FreeDiskAnalyzer/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
+using System.ComponentModel;
 using System.Windows;
+using System.Windows.Media.Animation;
+using FreeDiskAnalyzer.ViewModels;
 
 namespace FreeDiskAnalyzer;
 
@@ -7,5 +10,37 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        DataContextChanged += OnDataContextChanged;
+    }
+
+    private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+    {
+        if (e.OldValue is MainViewModel oldVm)
+        {
+            oldVm.PropertyChanged -= OnMainViewModelPropertyChanged;
+        }
+
+        if (e.NewValue is MainViewModel newVm)
+        {
+            newVm.PropertyChanged += OnMainViewModelPropertyChanged;
+        }
+    }
+
+    private void OnMainViewModelPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(MainViewModel.CurrentPage))
+        {
+            AnimatePageTransition();
+        }
+    }
+
+    private void AnimatePageTransition()
+    {
+        PageContent.Opacity = 0;
+        var animation = new DoubleAnimation(0, 1, new Duration(TimeSpan.FromSeconds(0.18)))
+        {
+            EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseOut }
+        };
+        PageContent.BeginAnimation(OpacityProperty, animation);
     }
 }
